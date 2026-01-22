f98283858a90b144a4c1f331017e94a3fb166752
ハチドリくん <yama.sakibill0630@gmail.com>
2026-01-22 15:15:30 +0900

    なこなこチャンネル

---
+++ Todo/src/main/java/com/example/demo/entity/Genre.java
@@ -0,0 +1,26 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "genres") // このクラスが「Task」テーブルに対応していることを指定
+@Getter // Lombokを使用してgetterメソッドを自動生成
+@Setter // Lombokを使用してsetterメソッドを自動生成
+public class Genre {
+	// タスクIDを主キーとして定義。自動的にインクリメントされる。
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // IDを自動生成するための戦略
+	@Column(name = "id") // データベースの「id」カラムにマッピング
+	private Integer id;
+
+	// タスクのタイトル。データベースの「title」カラムにマッピング
+	@Column(name = "genre")
+	private String genre;
+}
